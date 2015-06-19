7855c5ad3c8a862a11410153d27c839c6993aa12
Wart Claes <wart.claes@kbc.be>
2015-06-19 16:37:18 +0200

    cors and updated phone numbers

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express    	= require('express'),
     include     = require('include'),
 	bodyParser 	= require('body-parser'),
+	cors 		= require('cors'),
  	router 		= require('./router.js');
 
 var app = express();
@@ -8,6 +9,8 @@ var app = express();
 console.info('configuring body parser...');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(cors());
 
 console.info('configuring routes...');
 app.use('/API', router);
