2eff118c6f5d75e8f172723a1636e530658937c0
Peter Cosemans <peter.cosemans@euri.com>
2015-06-19 14:32:55 +0200

    Added simple server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+var port = process.env.PORT || process.env.DEFAULT_PORT || 9000
+var server = app.listen(port, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
